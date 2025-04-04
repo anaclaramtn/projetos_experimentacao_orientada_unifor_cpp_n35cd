9d3fd0793a2eeeb9e737e7b6ee77f4f3f282cc9d
Ana Clara <anaclaramtn@gmail.com>
2025-04-04 13:31:43 -0300

    Create traffic_lights.ino

---
+++ introducao_ao_arduino/traffic_lights.ino
@@ -0,0 +1,22 @@
+//Esta função seta pinos especificos e os determina como saídas;
+void setup(){
+    pinMode(8, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(6, OUTPUT);
+  }
+  
+  //Esta função roda o código infinitamente em loop, e não necessita de parâmetros
+  void loop() {
+    digitalWrite(8, HIGH) ;
+    digitalWrite(7, LOW);
+    digitalWrite(6, LOW);
+    delay(5000);
+    digitalWrite(8, LOW);
+    digitalWrite(7, HIGH);
+    digitalWrite(6, LOW);
+    delay(2000);
+    digitalWrite(4, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(6, LOW);
+    delay(5000);
+  }
